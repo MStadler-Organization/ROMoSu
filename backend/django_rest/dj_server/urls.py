717d62228e068cbcbcd0f5df2bedf5b5e39a3039
Marco Stadler <marco.stadler@jku.at>
2022-07-05 17:36:16 +0200

    dummy rest setup + doku

--- backend/django_rest/dj_server/urls.py
+++ backend/django_rest/dj_server/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from dj_server.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'hero', views.HeroViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('herolist/', views.hero_list),
 ]
