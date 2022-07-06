7a5fb1d9e19ea43d041a709610cfcf746e235970
Marco Stadler <marco.stadler@jku.at>
2022-07-06 10:04:04 +0200

    installed and tested angular materials

--- frontend/src/app/pages/home/home.module.ts
+++ frontend/src/app/pages/home/home.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
+import {HomeRoutingModule} from './home-routing.module';
+import {HomeComponent} from './home.component';
+import {MatSliderModule} from "@angular/material/slider";
 
 
 @NgModule({
@@ -10,8 +11,10 @@ import { HomeComponent } from './home.component';
     HomeComponent
   ],
   imports: [
+    MatSliderModule,
     CommonModule,
     HomeRoutingModule
   ]
 })
-export class HomeModule { }
+export class HomeModule {
+}
