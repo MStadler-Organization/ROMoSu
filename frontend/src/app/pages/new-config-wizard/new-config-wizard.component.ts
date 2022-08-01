9684523f04ecf5aa5c8555f89ea9e609ada4e102
Marco Stadler <marco.stadler@jku.at>
2022-08-01 09:47:32 +0200

    created possible sum selection

--- frontend/src/app/pages/new-config-wizard/new-config-wizard.component.ts
+++ frontend/src/app/pages/new-config-wizard/new-config-wizard.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from "@angular/forms";
+import {MatStepper} from "@angular/material/stepper";
+import {NewConfigWizardService} from "./new-config-wizard.service";
 
 @Component({
   selector: 'app-new-config-wizard',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-config-wizard.component.scss']
 })
 export class NewConfigWizardComponent implements OnInit {
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: [''],
+    // firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: [''],
+    // secondCtrl: ['', Validators.required],
+  });
 
-  constructor() { }
+  selectedSum: string = ''
 
-  ngOnInit(): void {
+  possibleSums: string[] = []
+
+  constructor(private _formBuilder: FormBuilder, public newConfigWizardService: NewConfigWizardService) {
   }
 
+  ngOnInit(): void {
+    this.newConfigWizardService.getSuMs().subscribe((possibleSums: string[]) => {
+      this.possibleSums = possibleSums.sort()
+    })
+  }
+
+  /***
+   * Called when step 1 next button is clicked
+   * @param sums the selection of the possible sum-list
+   * @param stepper the stepper mat component
+   */
+  validateSumSelection(sums: { selectedOptions: { selected: { value: string; }[]; }; }, stepper: MatStepper) {
+    this.selectedSum = sums.selectedOptions.selected[0]?.value
+    if (!this.selectedSum) {
+      // todo: show dialog or something
+    } else {
+      stepper.next()
+    }
+  }
 }
--- frontend/src/app/pages/new-config-wizard/new-config-wizard.module.ts
+++ frontend/src/app/pages/new-config-wizard/new-config-wizard.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { NewConfigWizardRoutingModule } from './new-config-wizard-routing.module';
-import { NewConfigWizardComponent } from './new-config-wizard.component';
+import {NewConfigWizardRoutingModule} from './new-config-wizard-routing.module';
+import {NewConfigWizardComponent} from './new-config-wizard.component';
+import {MatStepperModule} from "@angular/material/stepper";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatListModule} from "@angular/material/list";
 
 
 @NgModule({
@@ -11,7 +17,14 @@ import { NewConfigWizardComponent } from './new-config-wizard.component';
   ],
   imports: [
     CommonModule,
-    NewConfigWizardRoutingModule
+    NewConfigWizardRoutingModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule
   ]
 })
-export class NewConfigWizardModule { }
+export class NewConfigWizardModule {
+}
